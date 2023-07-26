b5fbd5b1c67a2b6ccd534d25c6aebcfc09f74c7c
Shani Mithani <shanimithani@gmail.com>
2023-07-26 17:24:34 -0400

    Parameterized values

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,30 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = process.argv[2] || 'JUL02';
+
+const queryString = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name = $1
+  ORDER BY teacher;
+`;
+
+const values = [cohortName];
+
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  })
+  .catch(err => console.error('query error', err.stack));
